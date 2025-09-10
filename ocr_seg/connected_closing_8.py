47d6e9951a76545e95aa79edc3e33c74be3b91b8
Nitesh Kakkar <kakkar.nitesh04@gmail.com>
2025-09-10 16:01:59 +0530

    rem closing to avoid wrong box

--- ocr_seg/connected_closing_8.py
+++ ocr_seg/connected_closing_8.py
@@ -31,7 +31,7 @@ cv2.imshow("Binarized Image (Global)", binarized_img)
 cv2.waitKey(0)
 
 
-num_labels, labels, stats, _ = cv2.connectedComponentsWithStats(binarized_img, connectivity=8)
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(binarized_img, connectivity=8)
 
 # keep only large components
 min_area = 1000  
@@ -45,34 +45,7 @@ for i in range(1, num_labels):  # skip background
 cv2.imshow("Filtered Large Components", refined)
 cv2.waitKey(0)
 
-
-shape = "rect"      
-ksize = (15, 10)      
-iterations = 1          
-
-
-shape_map = {
-    "rect": cv2.MORPH_RECT,
-    "ellipse": cv2.MORPH_ELLIPSE,
-    "cross": cv2.MORPH_CROSS
-}
-
-
-kernel = cv2.getStructuringElement(shape_map[shape], ksize)
-
-
-dilated = cv2.dilate(refined, kernel, iterations=iterations)
-
-
-closed_manual = cv2.erode(dilated, kernel, iterations=iterations)
-
-cv2.imshow("Manual Closing", closed_manual)
-cv2.waitKey(0)
-
-
-num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(closed_manual, connectivity=8)
-
-output = cv2.cvtColor(closed_manual, cv2.COLOR_GRAY2BGR)
+output = cv2.cvtColor(refined, cv2.COLOR_GRAY2BGR)
 
 min_area = 1000
 word_id = 0
@@ -81,7 +54,7 @@ for i in range(1, num_labels):
     x, y, w, h, area = stats[i]
     if area >= min_area:
         cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        word_crop = closed_manual[y:y+h, x:x+w]
+        word_crop = refined[y:y+h, x:x+w]
         word_id += 1
 
 cv2.imshow("Word Boxes (Connected Components)", output)
